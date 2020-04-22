23f42372e4f9e18923b693686d5c97231a33c8c2
Fernando Bueno Gutiérrez <fernando.buenogutierrez@wur.nl>
2020-04-22 12:34:37 +0200

    Delete example1.R

--- example1.R
+++
@@ -1,9 +0,0 @@
-#This is one of my script examples and it will be uploaded
-my_vec<-c(4,5,6)
-my_updated_vec<-my_vec*66
-print(my_updated_vec)
-#now I am modifying file directly https://github
-my_other_vec<-c(9,9,89)
-print(my_updated_vec-my_other_vec)
-#and now I am modifying from R and want to create a new branch. See if I can see the two versions in https
-print(paste0("myvector is ",my_updated_vec))
